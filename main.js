af30b9e432cd8e5436c19fc916a3c722c273863e
apinkpanda722 <iih907@gmail.com>
2020-07-23 18:17:29 +0900

    scroll to section

--- main.js
+++ main.js
@@ -4,6 +4,11 @@
 const navbar = document.querySelector("#navbar");
 const navbarHeight = navbar.getBoundingClientRect().height;
 
+function scrollIntoView(selector) {
+  const scrollTo = document.querySelector(selector);
+  scrollTo.scrollIntoView({ behavior: "smooth" });
+}
+
 document.addEventListener("scroll", () => {
   console.log(navbarHeight);
   if (window.scrollY > navbarHeight) {
@@ -16,13 +21,18 @@ document.addEventListener("scroll", () => {
 // Handle scrolling when tapping on the navbar menu
 
 const navbarMenu = document.querySelector(".navbar__menu");
-navbarMenu.addEventListener("click", (evt) => {
+navbarMenu.addEventListener("click", (event) => {
   const target = event.target;
   const link = target.dataset.link;
   if (link == null) {
     return;
   }
-  console.log(evt.target.dataset.link);
-  const scrollTo = document.querySelector(link);
-  scrollTo.scrollIntoView({ behavior: "smooth" });
+  scrollIntoView(link);
+});
+
+// Handle click on "contact me" button on home
+
+const homeContactBtn = document.querySelector(".home__contact");
+homeContactBtn.addEventListener("click", (evt) => {
+  scrollIntoView("#contact");
 });
